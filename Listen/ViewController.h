d0cd38ca4845d1b5e2314e7a6d02da03408982ee
chen <chenheard@163.com>
2016-12-12 21:48:11 +0800

    Initial Commit

---
+++ Listen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Listen
+//
+//  Created by water on 16/12/12.
+//  Copyright © 2016年 water. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Listen/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Listen
+//
+//  Created by water on 16/12/12.
+//  Copyright © 2016年 water. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
